0810dc821154567e82eaf9b096f6cf0f8a6685cb
Trensharo <44678477+Trensharo@users.noreply.github.com>
2023-02-04 11:27:23 -0500

    BaseAnimator Script Upload

---
+++ Assets/Scripts/BaseAnimator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseAnimator : MonoBehaviour
+{
+    Animator thisAnimator;
+
+    Vector2 deltaPos;
+    Vector2 oldPos = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaPos = (Vector2)(transform.position) - oldPos;
+
+        if (deltaPos.magnitude > .05f)
+        {
+            thisAnimator.SetFloat("XSpeed", deltaPos.x);
+            thisAnimator.SetFloat("YSpeed", deltaPos.y);
+        }
+
+        oldPos = transform.position;
+    }
+
+    public void SetRunning(bool newValue)
+    {
+        thisAnimator.SetBool("running", newValue);
+    }
+}
