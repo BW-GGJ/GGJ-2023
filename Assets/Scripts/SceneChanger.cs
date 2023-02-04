84c0b2c06880995351272d9f0ee44bf5702b9fa9
Trensharo <44678477+Trensharo@users.noreply.github.com>
2023-02-03 23:13:48 -0500

    Scene Changer update
    scenes added

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -11,6 +11,11 @@ public class SceneChanger : MonoBehaviour
     [SerializeField] string howToPlaySceneName;
     [SerializeField] string creditsSceneName;
     [SerializeField] string gameSceneName;
+    [SerializeField] string startTownSceneName;
+    [SerializeField] string shovelSnowSceneName;
+    [SerializeField] string campfireSceneName;
+    [SerializeField] string carryWaterSceneName;
+    [SerializeField] string finalTownSceneName;
 
     private void Awake()
     {
@@ -32,5 +37,26 @@ public class SceneChanger : MonoBehaviour
     public void LoadGameScene()
     {
         SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void LoadStartTownScene()
+    {
+        SceneManager.LoadScene(startTownSceneName);
+    }
+    public void LoadShovelSnowScene()
+    {
+        SceneManager.LoadScene(shovelSnowSceneName);
+    }
+    public void LoadCampfireScene()
+    {
+        SceneManager.LoadScene(campfireSceneName);
+    }
+    public void LoadCarryWaterScene()
+    {
+        SceneManager.LoadScene(carryWaterSceneName);
+    }
+    public void LoadFinishTownScene()
+    {
+        SceneManager.LoadScene(finalTownSceneName);
     }
 }
