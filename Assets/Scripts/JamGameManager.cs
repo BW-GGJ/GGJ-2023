39f96fcb483a90efed7e52d2719df50c5c77d1ad
Trensharo <44678477+Trensharo@users.noreply.github.com>
2023-02-05 15:33:26 -0500

    Boss stuff

--- Assets/Scripts/JamGameManager.cs
+++ Assets/Scripts/JamGameManager.cs
@@ -20,6 +20,5 @@ public class JamGameManager : MonoBehaviour
     void Start()
     {
         SceneChanger.instance.LoadMenuScene();
-        
     }
 }
--- Assets/Scripts/KettuBossAI.cs
+++ Assets/Scripts/KettuBossAI.cs
@@ -19,7 +19,7 @@ public class KettuBossAI : MonoBehaviour
     [SerializeField] GameObject lilyRootPrefab;
     float lilyTimer = 6.0f;
 
-    [SerializeField] float lilyTime = 10.0f;
+    [SerializeField] float lilyTime = 8.0f;
 
     [SerializeField] List<CinematicDialogElements> brotherChats = new List<CinematicDialogElements>();
 
@@ -45,8 +45,8 @@ public class KettuBossAI : MonoBehaviour
         if(lilyTimer >= lilyTime)
         {
             lilyTimer = 0f;
-            float ranX = Random.Range(-13, 13);
-            float ranY = Random.Range(-10, 10);
+            float ranX = Random.Range(-10, 10);
+            float ranY = Random.Range(-8, 8);
 
             Instantiate(lilyRootPrefab, new Vector3(transform.position.x + ranX, transform.position.y + ranY, 10), Quaternion.identity);
 
