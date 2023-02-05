3f2be595aad2e705c282579508939e3590210b40
RedIN3rds <redin3rds@gmail.com>
2023-02-05 01:55:57 -0500

    Music!

--- Assets/CampfireHUD.cs
+++ Assets/CampfireHUD.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class CampfireHUD : MonoBehaviour
 {
     public static CampfireHUD instance;
+    public bool overridenen;
     private void Awake()
     {
         if (!instance) instance = this;
@@ -13,6 +14,12 @@ public class CampfireHUD : MonoBehaviour
 
     public void Refresh() 
     {
+        if (overridenen) return;
         GetComponent<TMP_Text>().text = $"Place logs in the fire. ({StickManager.SticksCollected}/5)";
+    }
+    public void OverrideText(string text) 
+    {
+        overridenen = true;
+        GetComponent<TMP_Text>().text = text;
     }
 }
