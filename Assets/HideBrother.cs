5e424ff61ead694efddeabb414d8426dee9e0388
RedIN3rds <redin3rds@gmail.com>
2023-02-05 10:46:15 -0500

    Find Brother objective complete!

--- Assets/HideBrother.cs
+++ Assets/HideBrother.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class HideBrother : MonoBehaviour
 {
     [SerializeField] private GameObject brother;
+    [SerializeField] private GameObject Footsteps;
+    [SerializeField] private GameObject RoadBlocks;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (StickManager.SticksCollected == 5) 
         {
             brother.SetActive(false);
+            RoadBlocks.SetActive(false);
+
+            Footsteps.SetActive(true);
             CampfireHUD.instance.OverrideText("Find Brother.");
 
             Destroy(gameObject);
