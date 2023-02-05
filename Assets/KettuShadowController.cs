8597952975835f10395548383132db9f290d35c0
Trensharo <44678477+Trensharo@users.noreply.github.com>
2023-02-05 12:54:20 -0500

    Kettu Shadow

--- Assets/KettuShadowController.cs
+++ Assets/KettuShadowController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class KettuShadowController : MonoBehaviour
 {
+    [SerializeField] GameObject runAwayLocation;
+    [SerializeField] Animator kettuAnimator;
+
+    bool runninAway = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,18 @@ public class KettuShadowController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (runninAway)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, runAwayLocation.transform.position, 8 * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            kettuAnimator.SetTrigger("StartRunning");
+            runninAway = true;
+        }
     }
 }
